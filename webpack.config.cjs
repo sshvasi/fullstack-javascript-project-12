e9a8b560162a4d1cdb3fb06370de84dbec5518a3
Alexander Vasiliev <asvasi25@gmail.com>
2022-10-09 10:09:29 +0300

    chore: add aliases for pages and context dirs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -16,6 +16,8 @@ module.exports = {
     alias: {
       '@src': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src', 'components'),
+      '@context': path.resolve(__dirname, 'src', 'context'),
+      '@pages': path.resolve(__dirname, 'src', 'pages'),
     },
   },
   module: {
