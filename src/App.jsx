2f244a10b3d2ce927206a08f9f2c09ec56ed9aa5
Alexander Vasiliev <asvasi25@gmail.com>
2022-10-17 18:27:14 +0300

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,27 @@
-import { useContext } from 'react';
-import { Box, useTheme, IconButton, Typography } from '@mui/material';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import ColorModeContext from './context/ColorModeContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthProvider from '@components/AuthProvider';
+import ThemeProvider from '@components/ThemeProvider';
+import Layout from '@components/Layout';
+import LoginPage from '@pages/LoginPage';
+import SignupPage from '@pages/SignupPage';
+import NotFoundPage from '@pages/NotFoundPage';
+import Chat from '@pages/ChatPage';
 
-const App = () => {
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        width: '100%',
-        flexDirection: 'column',
-        gap: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.default',
-        color: 'text.primary',
-        borderRadius: 1,
-        p: 3,
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        {theme.palette.mode} mode
-        <IconButton
-          sx={{ ml: 1 }}
-          onClick={colorMode.toggleColorMode}
-          color="inherit"
-        >
-          {theme.palette.mode === 'dark' ? (
-            <Brightness7Icon />
-          ) : (
-            <Brightness4Icon />
-          )}
-        </IconButton>
-      </Box>
-      <Typography variant="body1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error rerum
-        suscipit veniam? Consequatur minus laudantium, doloribus, in ad fugit
-        quae ea voluptatem quasi ducimus deserunt, blanditiis ratione itaque
-        alias placeat adipisci nobis. Quia neque reprehenderit sunt animi, ullam
-        nulla minus quae reiciendis sapiente, fuga ratione iusto debitis omnis
-        similique aspernatur! Dolore cupiditate expedita tenetur ratione
-        necessitatibus, inventore nam facilis hic sapiente itaque corrupti
-        repellat ut mollitia dolor incidunt qui sed aut soluta error nulla? Ex,
-        ullam, ipsam dolorum error ipsa, ut vero laboriosam commodi qui enim
-        consequuntur suscipit iure molestias tempora velit neque necessitatibus
-        et quaerat magni perspiciatis officia cum!
-      </Typography>
-    </Box>
-  );
-};
+const App = () => (
+  <ThemeProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Chat />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </Router>
+  </ThemeProvider>
+);
 
 export default App;
