007a0619f79473f049dde1224db00a4efabfbe04
Alexander Vasiliev <asvasi25@gmail.com>
2022-11-08 04:03:05 +0300

    chore: configure proxy for dev server

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,6 @@
 const path = require('path');
 const { ProvidePlugin } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -17,63 +16,42 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    open: false,
+    proxy: {
+      '/': 'http://0.0.0.0:5001',
     },
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
         exclude: /node_modules/,
+        use: ['babel-loader'],
       },
       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
-          },
-        ],
-        include: /\.module\.css$/,
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
       },
       {
-        test: /\.s[ac]ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /\.module\.css$/,
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Chat',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+    }),
     new ProvidePlugin({
       React: 'react',
     }),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html',
-    }),
-    new MiniCssExtractPlugin(),
   ],
-  devtool: 'inline-source-map',
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-    open: false,
-    proxy: {
-      '/': {
-        target: 'http://0.0.0.0:3000',
-        router: () => 'http://0.0.0.0:5001',
-        changeOrigin: true,
-        logLevel: 'debug',
-      },
-    },
-  },
 };
