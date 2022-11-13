fdf2b64da0140078e91c9117e7b67a3d6954e018
Alexander Vasiliev <asvasi25@gmail.com>
2022-11-13 06:22:27 +0300

    feat: separate message and list

---
+++ src/components/Message.jsx
@@ -0,0 +1,39 @@
+import { ListItem, ListItemContent, Typography } from '@mui/joy';
+
+const Message = ({ username, content, byCurrentUser }) => (
+  <ListItem
+    sx={{
+      maxWidth: 500,
+      py: 1,
+      px: 2,
+      // alignSelf: byCurrentUser ? 'flex-end' : 'flex-start',
+      alignSelf: 'flex-start',
+      bgcolor: byCurrentUser ? 'background.currentUser' : 'background.componentBg',
+      border: '1px solid',
+      borderRadius: 24,
+      borderColor: 'divider',
+    }}
+  >
+    <ListItemContent>
+      <Typography
+        sx={{
+          fontSize: 'sm',
+          fontWeight: 'lg',
+          color: byCurrentUser ? 'text.currentUser' : 'text.primary',
+        }}
+      >
+        {username}
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: 'sm',
+          color: byCurrentUser ? 'text.currentUser' : 'text.primary',
+        }}
+      >
+        {content}
+      </Typography>
+    </ListItemContent>
+  </ListItem>
+);
+
+export default Message;
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,74 +1,46 @@
+import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { Box, List, ListItem, ListItemContent, Typography } from '@mui/joy';
+import { List } from '@mui/joy';
 
 import useScrollToBottom from '@/hooks/useScrollToBottom';
 import { useGetChannelsQuery, useGetMessagesQuery } from '@/slices/apiSlice';
+import Message from '@/components/Message';
 
 const Messages = () => {
-  const { username } = useSelector((state) => state.auth);
+  const { username: currentUsername } = useSelector((state) => state.auth);
   const { data: messages } = useGetMessagesQuery();
   const { data: channels } = useGetChannelsQuery();
 
-  const messagesListRef = useScrollToBottom(messages);
+  const messagesListRef = useScrollToBottom(messages, channels);
 
-  const activeChannelMessages = messages?.messages.filter((message) => {
-    return message.channelId === channels?.currentChannelId;
-  });
+  const activeChannelMessages = useCallback(
+    messages?.messages.filter((message) => {
+      return message.channelId === channels?.currentChannelId;
+    }),
+    [channels, messages],
+  );
 
   return (
-    <Box
+    <List
       sx={{
-        px: 3,
         flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        p: 3,
         overflow: 'auto',
       }}
       ref={messagesListRef}
     >
-      <List
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          '--List-item-radius': '24px',
-        }}
-      >
-        {activeChannelMessages?.map((message) => (
-          <ListItem
-            key={message.id}
-            sx={{
-              maxWidth: 500,
-              py: 1,
-              px: 2,
-              alignSelf: message.username === username ? 'flex-end' : 'flex-start',
-              bgcolor:
-                message.username === username ? 'background.currentUser' : 'background.componentBg',
-              border: '1px solid',
-              borderColor: 'divider',
-            }}
-          >
-            <ListItemContent key={message.id}>
-              <Typography
-                sx={{
-                  fontSize: 'sm',
-                  fontWeight: 'lg',
-                  color: message.username === username ? 'text.currentUser' : 'text.primary',
-                }}
-              >
-                {message.username}
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: 'sm',
-                  color: message.username === username ? 'text.currentUser' : 'text.primary',
-                }}
-              >
-                {message.content}
-              </Typography>
-            </ListItemContent>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+      {activeChannelMessages?.map(({ id, username, content }) => (
+        <Message
+          key={id}
+          username={username}
+          content={content}
+          byCurrentUser={username === currentUsername}
+        />
+      ))}
+    </List>
   );
 };
 
