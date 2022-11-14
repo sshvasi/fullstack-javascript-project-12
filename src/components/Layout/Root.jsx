7f97a6048fd96fd48e3c988db87c354baa2f12c1
Alexander Vasiliev <asvasi25@gmail.com>
2022-11-15 00:06:25 +0300

    fix: change sm breakpoint

--- src/components/Layout/Root.jsx
+++ src/components/Layout/Root.jsx
@@ -8,7 +8,7 @@ const Root = ({ sx = [], ...props }) => (
         display: 'grid',
         gridTemplateColumns: {
           xs: '1fr',
-          sm: 'minmax(64px, 200px) minmax(450px, 1fr)',
+          sm: 'minmax(64px, 250px) minmax(400px, 1fr)',
           md: 'minmax(160px, 300px) minmax(500px, 1fr)',
         },
         gridTemplateRows: '64px 1fr',
